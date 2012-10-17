64cd6a5ffdc32b1e86f67daf695330d9d8a41841
Esteban S. Rodriguez <tepenrodriguez@gmail.com>
2012-10-17 16:19:46 -0300

    refactor tests and helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
 
   #Returns the Gravatar (http://gravatar.com) for the given user.
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: '50x50', alt: user.name, class: "gravatar" })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, options )
   end
 end
